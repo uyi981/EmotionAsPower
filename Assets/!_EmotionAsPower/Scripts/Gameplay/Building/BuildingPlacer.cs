bddf5e8da22b1923653de78889551ee436db7831
Nghiem <thanhnghiem199@gmail.com>
2025-07-23 15:50:35 +0700

    fix conflict file BuildingPlacer

--- Assets/!_EmotionAsPower/Scripts/Gameplay/Building/BuildingPlacer.cs
+++ Assets/!_EmotionAsPower/Scripts/Gameplay/Building/BuildingPlacer.cs
@@ -1,7 +1,6 @@
 ﻿using System.ComponentModel;
 using Assets.__EmotionAsPower.Scripts.Gameplay.Building;
 using UnityEngine;
-
 public class BuildingPlacer : MonoBehaviour
 {
     [Header("Cấu hình Building Placer")]
@@ -13,64 +12,64 @@ public class BuildingPlacer : MonoBehaviour
     public SO_Building selectedBuilding;
 
 
-//    [ReadOnly(true)] 
-//    public GameObject blueprintInstance;
- 
+    [ReadOnly(true)]
+    public GameObject blueprintInstance;
 
 
-  
 
-//    void Update()
-//    {
-//        if (selectedBuilding != null)
-//        {
-//            HandleBlueprintFollowMouse();
-//            //if (Input.GetMouseButtonDown(0))
-//            //{
-//            //    TryPlaceBuilding();
-//            //}
-//        }
-//    }
 
-//    void HandleBlueprintFollowMouse()
-//    {
-        
-//        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
 
-//        Debug.DrawRay(ray.origin, ray.direction * 200f, Color.green);
+    void Update()
+    {
+        if (selectedBuilding != null)
+        {
+            HandleBlueprintFollowMouse();
+            if (Input.GetMouseButtonDown(0))
+            {
+                TryPlaceBuilding();
+            }
+        }
+    }
 
-//        if (Physics.Raycast(ray, out RaycastHit hit, 200f, placementLayer))
-//        {
-//            Debug.Log("Hit point: " + hit.point);
-//            if (blueprintInstance == null)
-//            {
-//                blueprintInstance = Instantiate(selectedBuilding.blueprintPrefab);
-//            }
-//            blueprintInstance.transform.position = hit.point;
-//        }
-//    }
+    void HandleBlueprintFollowMouse()
+    {
 
-//    //void TryPlaceBuilding()
-//    //{
-//    //    GameObject buildingObj = Instantiate(selectedBuilding.buildingPrefab, blueprintInstance.transform.position, Quaternion.identity);
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+        Debug.DrawRay(ray.origin, ray.direction * 200f, Color.green);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, 200f, placementLayer))
+        {
+            Debug.Log("Hit point: " + hit.point);
+            if (blueprintInstance == null)
+            {
+                blueprintInstance = Instantiate(selectedBuilding.blueprintPrefab);
+            }
+            blueprintInstance.transform.position = hit.point;
+        }
+    }
+
+    void TryPlaceBuilding()
+    {
+        GameObject buildingObj = Instantiate(selectedBuilding.buildingPrefab, blueprintInstance.transform.position, Quaternion.identity);
 
         // Truyền SO_Building cho script Building trên prefab
-        Building buildingScript = buildingObj.GetComponent<Building>();
+        Assets.__EmotionAsPower.Scripts.Gameplay.Building.Building buildingScript = buildingObj.GetComponent<Assets.__EmotionAsPower.Scripts.Gameplay.Building.Building>();
         if (buildingScript != null)
         {
             buildingScript.selectedBuilding = this;
         }
 
-//    //    Destroy(blueprintInstance);
+        Destroy(blueprintInstance);
 
-//    //}
+    }
 
 
-//    // Hàm này gọi khi chọn công trình từ Shop
-//    public void SelectBuilding(SO_Building building)
-//    {
-//        selectedBuilding = building;
-//        if (blueprintInstance != null)
-//            Destroy(blueprintInstance);
-//    }
-//}
+    // Hàm này gọi khi chọn công trình từ Shop
+    public void SelectBuilding(SO_Building building)
+    {
+        selectedBuilding = building;
+        if (blueprintInstance != null)
+            Destroy(blueprintInstance);
+    }
+}
